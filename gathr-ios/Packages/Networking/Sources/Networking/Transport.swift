da61442c92fba520b50d630ed546f62d0ea54449
frankolien <frankolien123@gmail.com>
2026-08-19 13:12:48 +0100

    feat(ios-net): add URLSessionTransport

--- gathr-ios/Packages/Networking/Sources/Networking/Transport.swift
+++ gathr-ios/Packages/Networking/Sources/Networking/Transport.swift
@@ -16,3 +16,22 @@ public protocol Transport: Sendable {
     func perform(_ request: URLRequest) async throws -> HTTPResponse
 }
 
+public struct URLSessionTransport: Transport {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    public func perform(_ request: URLRequest) async throws -> HTTPResponse {
+        let (data, response) = try await session.data(for: request)
+        guard let http = response as? HTTPURLResponse else {
+            throw GathrError.transport("the server did not return an HTTP response")
+        }
+        return HTTPResponse(
+            status: http.statusCode,
+            body: data,
+            requestId: http.value(forHTTPHeaderField: "X-Request-Id")
+        )
+    }
+}

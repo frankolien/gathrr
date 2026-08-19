0d8198cdefbe772fca8a620f629d919b8af82b76
frankolien <frankolien123@gmail.com>
2026-08-19 13:12:44 +0100

    feat(ios-models): add ErrorCode

--- gathr-ios/Packages/Models/Sources/Models/APIFailure.swift
+++ gathr-ios/Packages/Models/Sources/Models/APIFailure.swift
@@ -14,3 +14,32 @@ public struct APIFailure: Codable, Sendable, Hashable {
     public var requestId: String { error.requestId }
 }
 
+public enum ErrorCode: String, Sendable, CaseIterable {
+    case unauthenticated
+    case tokenReuseDetected = "token_reuse_detected"
+    case forbidden
+    case notFound = "not_found"
+    case inviteInvalid = "invite_invalid"
+    case inviteExpired = "invite_expired"
+    case inviteExhausted = "invite_exhausted"
+    case capacityExceeded = "capacity_exceeded"
+    case eventCancelled = "event_cancelled"
+    case eventEnded = "event_ended"
+    case plusOnesExceeded = "plus_ones_exceeded"
+    case validationFailed = "validation_failed"
+    case idempotencyConflict = "idempotency_conflict"
+    case rateLimited = "rate_limited"
+    case internalFailure = "internal"
+
+    public var requiresReauthentication: Bool {
+        self == .unauthenticated || self == .tokenReuseDetected
+    }
+
+    public var offersWaitlist: Bool {
+        self == .capacityExceeded
+    }
+
+    public var isRetryable: Bool {
+        self == .rateLimited || self == .internalFailure
+    }
+}

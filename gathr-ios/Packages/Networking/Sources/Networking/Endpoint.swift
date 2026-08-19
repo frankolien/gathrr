ab47410c0bf844bba586561bba4259475b7701eb
frankolien <frankolien123@gmail.com>
2026-08-19 13:12:48 +0100

    feat(ios-net): add HTTPMethod and Endpoint

---
+++ gathr-ios/Packages/Networking/Sources/Networking/Endpoint.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public enum HTTPMethod: String, Sendable {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+public struct Endpoint: Sendable {
+    public var method: HTTPMethod
+    public var path: String
+    public var query: [URLQueryItem]
+    public var body: Data?
+    public var requiresAuth: Bool
+    public var idempotencyKey: String?
+
+    public init(
+        method: HTTPMethod = .get,
+        path: String,
+        query: [URLQueryItem] = [],
+        body: Data? = nil,
+        requiresAuth: Bool = true,
+        idempotencyKey: String? = nil
+    ) {
+        self.method = method
+        self.path = path
+        self.query = query
+        self.body = body
+        self.requiresAuth = requiresAuth
+        self.idempotencyKey = idempotencyKey
+    }
+}
+

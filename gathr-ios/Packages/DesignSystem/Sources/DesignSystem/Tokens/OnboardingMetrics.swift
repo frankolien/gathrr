52f747487d6a2a69dd2690d5e9d92ea0c0901f41
frankolien <frankolien123@gmail.com>
2026-08-19 13:12:46 +0100

    feat(ios-design): add OnboardingMetrics

---
+++ gathr-ios/Packages/DesignSystem/Sources/DesignSystem/Tokens/OnboardingMetrics.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public enum OnboardingMetrics {
+    public static let gutter: CGFloat = 28
+    public static let copyGutter: CGFloat = 48
+    public static let posterHeight: CGFloat = 376
+    public static let posterGap: CGFloat = 16
+    public static let collageTopInset: CGFloat = 64
+    public static let copyGap: CGFloat = 10
+    public static let copyBottomInset: CGFloat = 8
+    public static let ctaGap: CGFloat = 14
+    public static let ctaTopInset: CGFloat = 26
+    public static let veilBlur: CGFloat = 34
+    public static let providerButtonHeight: CGFloat = 56
+    public static let driftPeriod: Double = 44
+    public static let bobRate: Double = 0.55
+    public static let bobHeight: CGFloat = 7
+    public static let copyDwell: Double = 3.6
+    public static let artworkToCopyGap: CGFloat = 40
+    public static let signUpArtworkInset: CGFloat = 40
+}

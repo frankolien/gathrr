76ea1d4edb1a0484841d37f2030b9d0f7b7f16b3
frankolien <frankolien123@gmail.com>
2026-08-19 13:12:47 +0100

    feat(ios-design): add UIColor

--- gathr-ios/Packages/DesignSystem/Sources/DesignSystem/Tokens/Palette.swift
+++ gathr-ios/Packages/DesignSystem/Sources/DesignSystem/Tokens/Palette.swift
@@ -86,3 +86,13 @@ public enum Palette {
     }
 }
 
+extension UIColor {
+    convenience init(hex: UInt32) {
+        self.init(
+            red: CGFloat((hex >> 16) & 0xFF) / 255,
+            green: CGFloat((hex >> 8) & 0xFF) / 255,
+            blue: CGFloat(hex & 0xFF) / 255,
+            alpha: 1
+        )
+    }
+}
